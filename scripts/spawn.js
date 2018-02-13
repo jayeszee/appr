18291b35e013526b6519cd09be321331ce4fdb05
Jack Zhang <jsz.zsj@gmail.com>
2018-02-13 09:43:41 -0800

    added a event listen for exit

--- scripts/spawn.js
+++ scripts/spawn.js
@@ -4,6 +4,7 @@ const childProcess = require('child_process');
  * Spawn a child process with a callback when the process exits
  */
 module.exports = function spawn(task, args, onClose) {
+  let closed = false;
   const child = childProcess.spawn(task, args, {
     stdio: 'inherit',
     env: process.env
@@ -13,6 +14,14 @@ module.exports = function spawn(task, args, onClose) {
     console.log(error);
   });
   child.on('close', code => {
+    if (closed) {return;}
     onClose(code);
+    closed = true;
+  });
+
+  child.on('exit', code => {
+    if (closed) {return;}
+    onClose(code);
+    closed = true;
   });
 };
